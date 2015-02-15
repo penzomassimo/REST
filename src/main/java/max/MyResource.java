e3430f31b615eb81f4babd4a8c7d7d70fe245eef
Massimo Penzo <penzo.massimo@gmail.com>
2015-02-15 15:12:40 -0600

    sendMail method is now receiving JSON data and sending email

--- src/main/java/max/MyResource.java
+++ src/main/java/max/MyResource.java
@@ -6,10 +6,7 @@ import com.google.gson.GsonBuilder;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -23,9 +20,7 @@ public class MyResource {
     @Path("man_catalog")
     @Produces("application/json")
     public String getManCatalog() {
-
         List<Product> myList = new ArrayList<Product>();
-
         Product p1 = new Product("Camera0","Best Camera0",101);
         Product p2 = new Product("Camera1","Best Camera1",220);
         Product p3 = new Product("Camera2","Best Camera2",002);
@@ -33,7 +28,6 @@ public class MyResource {
         Product p5 = new Product("Camera4","Best Camera4",004);
         Product p6 = new Product("Camera5","Best Camera4",005);
         Product p7 = new Product("Camera6","Best Camera4",006);
-
         myList.add(p1);
         myList.add(p2);
         myList.add(p3);
@@ -41,24 +35,10 @@ public class MyResource {
         myList.add(p5);
         myList.add(p6);
         myList.add(p7);
-
-
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-
         String json = gson.toJson(myList);
-
-
-
         return json;
-
-
-        /*Product p1 = new Product("Camera","The best camera",200);
-        System.out.println("the product has been created");
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        String json = gson.toJson(p1);
-        return json;*/
     }
 
     @GET
@@ -137,23 +117,25 @@ public class MyResource {
         return "problem";
     }
 
-    @GET
+    @POST
     @Path("send_email")
+    @Consumes("application/json")
     @Produces("application/json")
-    public String sendMail() {
+    public String sendMail(final String jsonData) {
+
         Properties props = new Properties();
         Session session = Session.getInstance(props);
         MimeMessage msg = new MimeMessage(session);
         Transport t = null;
         try {
             Address massimo = new InternetAddress("penzo.massimo@gmail.com", "CHUCKY");
-            Address alma = new InternetAddress("almavcarreras@gmail.com");
-            msg.setText("Resistance is futile. You will be assimilated!");
+            Address alma = new InternetAddress("penzo.massimo@gmail.com");
+            msg.setText(jsonData);
             msg.setFrom(massimo);
             msg.setRecipient(Message.RecipientType.TO, alma);
-            msg.setSubject("You must comply.");
+            msg.setSubject("TEST - You must comply.");
             t = session.getTransport("smtps");
-            t.connect("smtp.gmail.com", "penzo.massimo@gmail.com", "password");
+            t.connect("smtp.gmail.com", "penzo.massimo@gmail.com", "electrica913");
             t.sendMessage(msg, msg.getAllRecipients());
             return "email sent";
         } catch (MessagingException | UnsupportedEncodingException ex) {
